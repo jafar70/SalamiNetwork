2ecc1edd073a83e1678367a1ebc28367ec4a5d9e
jafsalami <jafsalami@hotmail.co.uk>
2019-10-22 17:26:55 -0400

    Added slider@

--- assets/css/watch.sh
+++ assets/css/watch.sh
@@ -3,7 +3,7 @@
 # Change `style.css` or `style.min.css` to whatever you would like your compiled
 # stylesheet to be called. Do not rename `style.scss` or alter references to it.
 
-sass --watch style.scss:style.min.css --style compressed -E "UTF-8"
+sass --watch style.scss:style.min.css --style "UTF-8"
 
 
 exit 0
